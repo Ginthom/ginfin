845fb33d8fcbcb5e92a690d8fe236f5ae5e2dcdd
Thomas Gingele <thomas@gingele.de>
2022-04-02 22:14:07 +0200

    Test class

--- src/ginfin.cpp
+++ src/ginfin.cpp
@@ -1,4 +1,13 @@
 #include <prehdr.h>
+#include <entry.h>
 
-int main() {
+int main(int argc, char** argv) {
+	float input;
+	std::cin >> input;
+
+	Entry entry = Entry();
+	entry.set_amount(input);
+	float output = entry.get_amount();
+
+	std::cout << "Output: " << output << std::endl;
 }
