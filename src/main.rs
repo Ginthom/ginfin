f64bfc76cd65219fc45a8f6a70e95f6bf39ab071
Thomas Gingele <thomas@gingele.de>
2022-04-03 00:55:20 +0200

    Add basic ideas

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn main() {
+    println!("❱ Ginfin ❰");
+
+    //print menu or something
+    //Add 'entries'
+    //fill up entries with values
+    //write entries to file
+    //load entries from file
+    //display entries
+    //calulate graph for
+    //  week
+    //  month
+    //  year
+    //display graph
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .unwrap();
+}
