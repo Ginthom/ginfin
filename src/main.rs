cbba5815b0092a735b116cba3ad59b85a9e96ce2
Thomas Gingele <thomas@gingele.de>
2022-04-06 00:16:00 +0200

    Add entry creation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ struct Entry {
     time: String,
 }
 
-pub fn create_entry() {
+struct History {
+    entries: Vec<Entry>,
+}
+
+fn create_entry(mut history: History) {
     let amount_new = input_reader::input_f32("Enter amount: ");
     let time_new = input_reader::input_string("Enter time: ");
 
@@ -14,25 +18,25 @@ pub fn create_entry() {
         time: time_new
     };
 
-    //Do something with entry i guess
+    history.entries.push(entry);
 }
 
-pub fn view_entries() {}
+fn view_entries(history: History) {}
 
-pub fn handle_input() {
+fn handle_input(history: History) {
     let input: i32 = input_reader::input_i32("Input: ");
     println!("You used option {}", input);
 
     match input {
-    1 => create_entry(),
-    2 => view_entries(),
+    1 => create_entry(history),
+    2 => view_entries(history),
     3 => std::process::exit(0),
     -1 => (),
     _ => println!("Invalid input")
     };
 }
 
-pub fn display_menu() {
+fn display_menu() {
     println!("❱ Ginfin ❰\n");
     println!("Options:\n1) New Entry\n2) View Entries\n3) Exit\n");
 
@@ -40,9 +44,16 @@ pub fn display_menu() {
 
 fn main() {
 
+    //Create history
+    let history = History {
+        entries:  Vec::new()
+    };
+
+    //Load history from file
+
     //print menu or something
     display_menu();
-    handle_input();
+    handle_input(history);
 
     //Add 'entries'
     //fill up entries with values
