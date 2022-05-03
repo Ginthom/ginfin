e708b4cd0c0a22d675e1f29b02f8866d48da70e9
Thomas Gingele <thomas@gingele.de>
2022-05-03 20:34:12 +0200

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod ginfin;
 use crate::ginfin::engine::Grid;
 
+struct grid {
+    pub width: u32,
+    pub height: u32,
+    pub grid: [[char; width]; height]
+}
+
 fn main() {
     let grid = Grid::new();
 
