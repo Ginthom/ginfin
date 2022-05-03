e04a5ff5234abe75f8b2e0f10a0845547fa416f0
Ginthom <thomas@gingele.de>
2022-05-03 10:49:23 +0200

    Add terminal size wrapper

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 use terminal_size::{Width, Height, terminal_size};
 
-fn main() {
-    let size = terminal_size();
+pub struct Dimension {
+    width:  u16,
+    height: u16
+}
 
-    if let Some((Width(w), Height(h))) = size {
-        println!("Terminal Size: {}x/{}y", w, h);
+pub fn get_dimensions() -> Result<Dimension, String> {
+    let term_size = terminal_size();
+    let mut dim   = Dimension{width: 0, height: 0};
+
+    if let Some((Width(x), Height(y))) = term_size {
+        dim.width  = x;
+        dim.height = y;
     }
+
+    match dim.width  > 0 
+       && dim.height > 0 {
+        true  => Ok(dim),
+        false => Err("Failed to get terminal size!".to_string())
+    }
+}
+
+fn main() {
+    let dim = match get_dimensions() {
+        Ok(result) => result,
+        Err(msg) => panic!("{}", msg)
+    };
+
+    println!("Terminal Size: {}x/{}y", dim.width, dim.height);
 
 }
