2fc683bcf8f3a9dbb2d95508b9291d5bd79dae79
Ginthom <thomas@gingele.de>
2022-05-04 12:52:03 +0200

    Coord adjustment for example line

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ fn main() {
     grid.set_rectangle(10, 10, 30, 30);
     grid.set_hline(5, 25, 40);
     grid.set_text(11, 11, "This is a box".to_string());
-    grid.set_hline(5, 5, 1000);
+    grid.set_hline(0, 5, 1000);
     draw(grid);
 }
