2ef85a87c5466e41ee8f2a41c7536fc3135a39fc
Ginthom <thomas@gingele.de>
2022-05-10 11:41:32 +0200

    Add thin lines

--- src/ginfin.rs
+++ src/ginfin.rs
@@ -2,19 +2,34 @@ pub mod engine {
     use terminal_size::{Width, Height, terminal_size};
     use std::process::Command;
 
-    struct Lines{}
-    impl Lines {
-        const D_HOR: char = '═';
-        const D_VER: char = '║';
-        const D_CRS: char = '╬'; 
-        const D_UDR: char = '╠';
-        const D_UDL: char = '╣';
-        const D_DLR: char = '╦';
-        const D_ULR: char = '╩';
-        const D_CDR: char = '╔';
-        const D_CDL: char = '╗';
-        const D_CUR: char = '╚';
-        const D_CUL: char = '╝';
+    struct DOUBLE_LINES{}
+    impl DOUBLE_LINES{
+        const HOR: char = '═';
+        const VER: char = '║';
+        const CRS: char = '╬'; 
+        const UDR: char = '╠';
+        const UDL: char = '╣';
+        const DLR: char = '╦';
+        const ULR: char = '╩';
+        const CDR: char = '╔';
+        const CDL: char = '╗';
+        const CUR: char = '╚';
+        const CUL: char = '╝';
+    }
+
+    struct THIN_LINES{}
+    impl THIN_LINES{
+        const HOR: char = '─';
+        const VER: char = '│';
+        const CRS: char = '┼'; 
+        const UDR: char = '├';
+        const UDL: char = '┤';
+        const DLR: char = '┬';
+        const ULR: char = '┴';
+        const CDR: char = '┌';
+        const CDL: char = '┐';
+        const CUR: char = '└';
+        const CUL: char = '┘';
     }
 
     pub struct Dimension {
@@ -88,8 +103,8 @@ pub mod engine {
         pub fn set_hline(&mut self, x: usize, y: usize, length: usize) {
             for i in 0..length {
                 match self.get_pixel(x+i, y) {
-                    Lines::D_VER => self.set_pixel(x+i, y, Lines::D_CRS),
-                    _            => self.set_pixel(x+i, y, Lines::D_HOR)
+                    DOUBLE_LINES::VER => self.set_pixel(x+i, y, DOUBLE_LINES::CRS),
+                    _            => self.set_pixel(x+i, y, DOUBLE_LINES::HOR)
                 }
             }
         }
@@ -97,17 +112,17 @@ pub mod engine {
         pub fn set_vline(&mut self, x: usize, y: usize, length: usize) {
             for i in 0..length {
                 match self.get_pixel(x, y+i) {
-                    Lines::D_HOR => self.set_pixel(x, y+i, Lines::D_CRS),
-                    _            => self.set_pixel(x, y+i, Lines::D_VER)
+                    DOUBLE_LINES::HOR => self.set_pixel(x, y+i, DOUBLE_LINES::CRS),
+                    _            => self.set_pixel(x, y+i, DOUBLE_LINES::VER)
                 }
             }
         }
 
         pub fn set_rectangle(&mut self, x: usize, y: usize, width: usize, height: usize) {
-            self.set_pixel(x, y, Lines::D_CDR);
-            self.set_pixel(x+width, y, Lines::D_CDL);
-            self.set_pixel(x, y+height, Lines::D_CUR);
-            self.set_pixel(x+width, y+height, Lines::D_CUL);
+            self.set_pixel(x, y, DOUBLE_LINES::CDR);
+            self.set_pixel(x+width, y, DOUBLE_LINES::CDL);
+            self.set_pixel(x, y+height, DOUBLE_LINES::CUR);
+            self.set_pixel(x+width, y+height, DOUBLE_LINES::CUL);
             self.set_hline(x+1, y, width-1);
             self.set_hline(x+1, y+height, width-1);
             self.set_vline(x, y+1, height-1);
