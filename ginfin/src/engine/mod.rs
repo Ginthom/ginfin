7b762fccdeb1004de31264f4e0abfdf9795b6011
Ginthom <thomas@gingele.de>
2022-05-31 21:57:33 +0200

    Add init for grid

---
+++ ginfin/src/engine/mod.rs
@@ -0,0 +1,29 @@
+use terminal_size::{Width, Height, terminal_size};
+
+pub struct Grid {
+    width:  usize,
+    height: usize,
+    pixel: Vec<Vec<char>>
+}
+
+impl Grid {
+    fn init_dimensions(&mut self) {
+        if let Some((Width(x), Height(y))) = terminal_size() {
+            self.width  = x as usize;
+            self.height = y as usize;
+        }
+    }
+
+    fn init_grid(&mut self) {
+        self.pixel = Vec::<Vec<char>>::new();
+
+        for i in 1..self.height {
+            self.pixel.push(Vec::<char>::new());
+        }
+    }
+
+    pub fn new(&mut self) {
+        self.init_dimensions();
+        self.init_grid();
+    }
+}
